3beeab8135dcd3e9ca5e418c3a97285ca2e4cf81
Jorge Cano <jorgeucano@gmail.com>
2017-07-14 11:00:37 -0300

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 // material
 import 'hammerjs';
@@ -9,18 +10,26 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdButtonModule} from '@angular/material';
 
 
-
 // components
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+//router
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule,
 
     // material components
